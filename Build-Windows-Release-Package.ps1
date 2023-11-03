00a3ae3c1b183848bb931a6a33947141f82aba16
tanguband <tanguband@gmail.com>
2023-11-03 21:27:51 +0900

    windows関係ファイル tang表記

--- Build-Windows-Release-Package.ps1
+++ Build-Windows-Release-Package.ps1
@@ -11,7 +11,7 @@ $Env:Path = $msbuild_path + ";" + $Env:Path
 
 function BuildPackage ($package_name, $package_unique_files, $build_conf) {
     # バイナリをリビルド
-    MSBuild.exe .\Hengband\Hengband.sln /t:Rebuild /p:Configuration=$build_conf
+    MSBuild.exe .\Tangband\Tangband.sln /t:Rebuild /p:Configuration=$build_conf
 
     if ($LASTEXITCODE -ne 0) {
         # ビルド失敗ならスクリプトを中断する
@@ -21,16 +21,16 @@ function BuildPackage ($package_name, $package_unique_files, $build_conf) {
     # 作業用テンポラリフォルダ
     $tempDir = New-TemporaryFile | ForEach-Object { Remove-Item $_; New-Item $_ -ItemType Directory }
 
-    $hengbandDir = Join-Path $tempDir $package_name
-    New-Item $hengbandDir -ItemType Directory
+    $tangbandDir = Join-Path $tempDir $package_name
+    New-Item $tangbandDir -ItemType Directory
 
     # 必要なファイルをコピーして、その中で不要になりえるものを削除
-    Copy-Item -Verbose -Path .\Hengband.exe, .\readme_angband, .\THIRD-PARTY-NOTICES.txt -Destination $hengbandDir
-    Copy-Item -Verbose -Path $package_unique_files -Destination $hengbandDir
-    Copy-Item -Verbose -Recurse -Path .\lib -Destination $hengbandDir -Exclude Makefile.am, *.raw, .gitattributes
-    Copy-Item -Verbose -Path .\lib\apex\h_scores.raw -Destination $hengbandDir\lib\apex
-    Remove-Item -Verbose -Exclude delete.me -Recurse -Path $hengbandDir\lib\save\*, $hengbandDir\lib\user\*
-    Remove-Item -Verbose -Exclude music.cfg, readme.txt, *.mp3 -Path $hengbandDir\lib\xtra\music\*
+    Copy-Item -Verbose -Path .\Tangband.exe, .\readme_angband, .\THIRD-PARTY-NOTICES.txt -Destination $tangbandDir
+    Copy-Item -Verbose -Path $package_unique_files -Destination $tangbandDir
+    Copy-Item -Verbose -Recurse -Path .\lib -Destination $tangbandDir -Exclude Makefile.am, *.raw, .gitattributes
+    Copy-Item -Verbose -Path .\lib\apex\h_scores.raw -Destination $tangbandDir\lib\apex
+    Remove-Item -Verbose -Exclude delete.me -Recurse -Path $tangbandDir\lib\save\*, $tangbandDir\lib\user\*
+    Remove-Item -Verbose -Exclude music.cfg, readme.txt, *.mp3 -Path $tangbandDir\lib\xtra\music\*
 
     # zipアーカイブ作成
     $package_path = Join-Path $(Get-Location) "${package_name}.zip"
@@ -41,6 +41,6 @@ function BuildPackage ($package_name, $package_unique_files, $build_conf) {
 }
 
 # 日本語版
-BuildPackage -package_name Hengband-$Version-jp -package_unique_files .\readme.md, .\autopick.txt -build_conf Release
+BuildPackage -package_name Tangband-$Version-jp -package_unique_files .\readme.md, .\autopick.txt -build_conf Release
 # 英語版
-BuildPackage -package_name Hengband-$Version-en -package_unique_files .\readme-eng.md, .\autopick_eng.txt -build_conf English-Release
+BuildPackage -package_name Tangband-$Version-en -package_unique_files .\readme-eng.md, .\autopick_eng.txt -build_conf English-Release
