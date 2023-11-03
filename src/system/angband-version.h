058e3f1156fc7fe3a0241dece7d6ee7b0f5137ce
tanguband <tanguband@gmail.com>
2023-11-03 23:01:27 +0900

    Update angband-version.h

--- src/system/angband-version.h
+++ src/system/angband-version.h
@@ -20,7 +20,7 @@ constexpr std::string_view ROOT_VARIANT_NAME("Hengband");
  * @brief セーブファイル上のバージョン定義 / "Savefile Version Number" for Hengband
  * @details v1.1.1以上にのみ適用
  */
-#define H_VER_MAJOR 3 //!< ゲームのバージョン定義(メジャー番号)
+#define H_VER_MAJOR 23 //!< ゲームのバージョン定義(メジャー番号)
 #define H_VER_MINOR 0 //!< ゲームのバージョン定義(マイナー番号)
 #define H_VER_PATCH 1 //!< ゲームのバージョン定義(パッチ番号)
 #define H_VER_EXTRA 0 //!< ゲームのバージョン定義(エクストラ番号)
@@ -28,7 +28,7 @@ constexpr std::string_view ROOT_VARIANT_NAME("Hengband");
 /*!
  * @brief セーブファイルのバージョン(3.0.0から導入)
  */
-constexpr uint32_t SAVEFILE_VERSION = 19;
+constexpr uint32_t SAVEFILE_VERSION = 23;
 
 /*!
  * @brief バージョンが開発版が安定版かを返す(廃止予定)
